6b683b4ceb517909de8876c3b7ffd7e9550920e9
nencyahir <125643703+nencyahir@users.noreply.github.com>
2023-03-27 17:38:18 +0530

    Update routes.rb

--- mailer/config/routes.rb
+++ mailer/config/routes.rb
@@ -2,16 +2,12 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
-  # # root to: "sessions#login"
-  # get 'sessions/login', to:'sessions#login'
+
   get 'products/search', to: 'products#search'
   resources :products
   root "users#new"
-  # root "products#new"
-  # get '/login', to: 'sessions#create'
-  # post '/login', to: 'sessions#create'
-  resources :users, only: [:new, :create, :update, :edit, :show, :destroy]
+
+  resorces :users
 
  
   
